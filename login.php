f90bd0e96e1705f3f4adf98ad339674b0ac51b20
Adil <86008926+adiltofutofu@users.noreply.github.com>
2023-10-22 03:42:47 +0600

    Create login.php

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>7884526f</title>
+</head>
+<body>
+    <h1>Please Log In</h1>
+    <form method="post">
+        <label for="who">User Name</label>
+        <input type="text" name="who" id="who" size="40"><br>
+        <label for="pass">Password</label>
+        <input type="password" name="pass" id="pass" size="40"><br>
+        <input type="submit" value="Log In">
+    </form>
+</body>
+</html> 
